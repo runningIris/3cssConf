0750bbc03eb8fd7350707576b795f5534663e73b
runningIris <irisrunning@foxmail.com>
2016-12-22 00:19:32 +0800

    小胡子动起来了

--- wentin/beard/main.js
+++ wentin/beard/main.js
@@ -1,4 +1,14 @@
 (function(){
-	let beards = document.getElementsByClassName('beard');
-	console.info(beards)
+
+	const beards = document.getElementsByClassName('beard');
+	document.addEventListener('mousemove', (e) => {
+		let x = e.clientX;
+		let y = e.clientY;
+		console.log(`(${x}, ${y})`);
+		if(y>150 && y<450){
+			beards[0].style.transform = `translate(400px, 300px) rotate(${(y-300) * 0.2}deg)`;
+			beards[1].style.transform = `translate(250px, 300px) scaleX(-1) rotate(${(y-300) * 0.2}deg)`;
+		}
+	})
+
 })();
