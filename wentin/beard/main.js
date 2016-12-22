bce81ccf5bc65316bbcf9edcc0f30500e2eaf727
runningIris <irisrunning@foxmail.com>
2016-12-22 14:28:58 +0800

    beard 字体样式

--- wentin/beard/main.js
+++ wentin/beard/main.js
@@ -6,8 +6,8 @@
 		let y = e.clientY;
 		console.log(`(${x}, ${y})`);
 		if(y>150 && y<450){
-			beards[0].style.transform = `translate(400px, 300px) rotate(${(y-300) * 0.2}deg)`;
-			beards[1].style.transform = `translate(250px, 300px) scaleX(-1) rotate(${(y-300) * 0.2}deg)`;
+			beards[0].style.transform = `translate(600px, 300px) rotate(${(y-300) * 0.2}deg)`;
+			beards[1].style.transform = `translate(450px, 300px) scaleX(-1) rotate(${(y-300) * 0.2}deg)`;
 		}
 	})
 
