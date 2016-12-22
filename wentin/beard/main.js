6304018ef363a81532a01e720d745e2b508edae8
runningIris <irisrunning@foxmail.com>
2016-12-22 15:05:41 +0800

    移动端兼容

--- wentin/beard/main.js
+++ wentin/beard/main.js
@@ -1,9 +1,11 @@
 (function(){
 
 	const beards = document.getElementsByClassName('beard');
-	document.addEventListener('mousemove', (e) => {
-		let x = e.clientX;
-		let y = e.clientY;
+	const move = 'ontouchmove' in document ? 'touchmove' : 'mousemove';
+	document.addEventListener(move, (e) => {
+		const ev = 'ontouchmove' in document ? e.touches[0] : e;
+		let x = ev.pageX;
+		let y = ev.pageY;
 		console.log(`(${x}, ${y})`);
 		if(y>150 && y<450){
 			beards[0].style.transform = `translate(600px, 300px) rotate(${(y-300) * 0.2}deg)`;
